f08692335aa793d2be06b3ae3f969bbbd16ece22
jeri <diltsjeri@gmail.com>
2020-12-10 11:35:55 -0500

    overlayed pizza name and centered absoulte div of undefined width

--- pizza/src/components/CreatedPizzaView.jsx
+++ pizza/src/components/CreatedPizzaView.jsx
@@ -1,23 +1,21 @@
-import cheesePizza from "../img/cheesePizzaCropped.png";
 import AnimationsMain from "./AnimationsMain";
+import "../css/CreatedPizzaView.css";
+import cheesePizza from "../img/cheesePizzaCropped.png";
 
 function CreatedPizzaView(props){
     // console.log(props.pizza.fields);
-    // I think this cheese pizza needs to only be rendered once in the component above
     return <div> 
         <div className="container">
-            <img
-            className="cheesePizza"
-            src={cheesePizza}
-            alt="cheese pizza"
-            ></img>
+            <img className="cheesePizza" src={cheesePizza} alt="cheese pizza"></img>
             {props.pizza.fields.basil && <AnimationsMain topping="basil" />}
             {props.pizza.fields.blackOlives && <AnimationsMain topping="blackOlives" />}
             {props.pizza.fields.greenPepper && <AnimationsMain topping="greenPepper" />}
             {props.pizza.fields.mushrooms && <AnimationsMain topping="mushroom" />}
             {props.pizza.fields.pepperoni && <AnimationsMain topping="pepperoni" />}
             {props.pizza.fields.pineapple && <AnimationsMain topping="pineapple" />}
-            <div className="pizzaName">{props.pizza.fields.name}</div>
+            <div className="pizzaNameContainer">
+                <div className="pizzaName">{props.pizza.fields.name}</div>
+            </div>
         </div>    
     </div>
 }
--- pizza/src/components/GalleryPage.jsx
+++ pizza/src/components/GalleryPage.jsx
@@ -1,35 +1,34 @@
 import axios from "axios";
 import { baseURL, config } from "../services";
 import { useState, useEffect } from "react";
-import CreatedPizzaView from "./CreatedPizzaView"
+import CreatedPizzaView from "./CreatedPizzaView";
 
 function GalleryPage() {
+  const [pizzas, setPizzas] = useState([]);
 
-    const [pizzas, setPizzas] = useState([]);
+  // Allows page to update when data is posted without refreshing the page
+  const [toggleFetch, setToggleFetch] = useState(false);
 
-    // Allows page to update when data is posted without refreshing the page
-    const [toggleFetch, setToggleFetch] = useState(false);
-
-    // will only run when things change
-    useEffect(() => {
-
+  // will only run when things change
+  useEffect(() => {
     // query modified to get 5 latest pizzas
     const getPizzas = async () => {
-      const resp = await axios.get(`${baseURL}?maxRecords=5&&sort%5B0%5D%5Bfield%5D=createdTime&&sort%5B0%5D%5Bdirection%5D=desc`, config);
+      const resp = await axios.get(
+        `${baseURL}?maxRecords=5&&sort%5B0%5D%5Bfield%5D=createdTime&&sort%5B0%5D%5Bdirection%5D=desc`,
+        config
+      );
       setPizzas(resp.data.records);
     };
     getPizzas();
   }, [toggleFetch]);
 
-    return (
-    <div>{
-      pizzas.map((pizza, i)=>(
-        <CreatedPizzaView key={i} pizza={pizza}/>
-      ))
-    }
-      
+  return (
+    <div>
+      {pizzas.map((pizza, i) => (
+        <CreatedPizzaView key={i} pizza={pizza} />
+      ))}
     </div>
-    )
+  );
 }
 
 export default GalleryPage;
